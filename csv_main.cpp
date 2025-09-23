378e7e2fa2a000b7cc47a8d283d669385d87c26d
Samson Shields <80023262+samsonshields@users.noreply.github.com>
2025-09-23 11:53:57 -0400

    Update csv_main.cpp

--- csv_main.cpp
+++ csv_main.cpp
@@ -11,42 +11,40 @@ using namespace std;
 void find(string serial);
 
 int main(){
-  string serial;
-  cout << "Input serial: " << endl;
-  find(serial);
-  return 0;
+	string serial;
+	cout << "Input serial: " << endl;
+	cin >> serial;
+	find(serial);
+	return 0;
 }
 
-void find(int serial){
-	string path = "filepath here";
+void find(string serial){
+	string path = "/Personal/";
 	vector<string> instances;
 	try {
 		for(const auto& entry : filesystem::directory_iterator(path)){
-			if(filesystem::is_regular_file(entry.status())){
+			if(entry.is_regular_file() && entry.path().extension() == ".csv"){
 				ifstream file(entry.path().filename());
 				if(!file.is_open){
 					cerr << "Unable to open file, exiting." << endl;
-					return 0;
 				}
 				string line;
-				while(getline(file, line)){
+				while(getline(file,line)){
 					istringstream iss(line);
 					string field;
-					getline(iss, field, ',');
-					getline(iss, field, ',');
-					getline(iss, field, ',');
+					getline(iss,field,',');
+					getline(iss,field,',');
+					getline(iss,field,',');
 					if(field == serial){
 						instances.push_back(file);
 					}
 				}
-				
 			} else {
-				cout << "Irregular file, exiting." << endl;
-        return 0;
+				cout << "No csv files found, exiting." << endl;
 			}
 		}
 		if(instances.size() > 0){
-			cout << "Serial found in " << instances.size() << " other surplus sheets:" << endl;
+			cout << "Serial found in " << instances.size() << " other surplus sheets: " << endl;
 			for(int i = 0; i < instances.size(); i++){
 				cout << instances[i] << endl;
 			}
@@ -57,6 +55,3 @@ void find(int serial){
 		cerr << "Error accessing this directory: " << e.what() << endl;
 	}
 }
-
-
-
